f28fc270e9803c434318375bbdae7bd68ccdc161
root <root@backup01.melb.axxin.com>
2014-01-17 14:54:40 +1100

    Added more detail about the hosts the target directories are being synced from/to.

--- backuppc-sync.sh
+++ backuppc-sync.sh
@@ -38,7 +38,7 @@ echo "Stopping BackupPC..." 2>&1 | $TEE_BIN -a $LOG_PATH
 $BACKUPPC_INITD stop 2>&1 | $TEE_BIN -a $LOG_PATH
 
 # $CONFIG_DIR sync
-echo "Syncing" $CONFIG_DIR "..." 2>&1 | $TEE_BIN -a $LOG_PATH
+echo "Syncing the directory" $CONFIG_DIR "from" `hostname` "to $REMOTE_SERVER..." 2>&1 | $TEE_BIN -a $LOG_PATH
 $RSYNC_BIN -avH --delete -e $SSH_BIN' -p '$SSH_PORT $CONFIG_DIR $SSH_USER@$REMOTE_SERVER:$CONFIG_DIR 2>&1 | $TEE_BIN -a $LOG_PATH
 # Reinstate the remote backup specific configuration file
 $SSH_BIN -p $SSH_PORT $SSH_USER@$REMOTE_SERVER cp $CONF_ORIG $CONF_FILE 2>&1 | $TEE_BIN -a $LOG_PATH
@@ -46,7 +46,7 @@ $SSH_BIN -p $SSH_PORT $SSH_USER@$REMOTE_SERVER cp $CONF_ORIG $CONF_FILE 2>&1 | $
 echo "" 2>&1 | $TEE_BIN -a $LOG_PATH
 
 # $DATA_DIR sync
-echo "Syncing" $DATA_DIR "..." 2>&1 | $TEE_BIN -a $LOG_PATH
+echo "Syncing the directory" $DATA_DIR "from" `hostname` "to $REMOTE_SERVER..." 2>&1 | $TEE_BIN -a $LOG_PATH
 $RSYNC_BIN -avH --delete -e $SSH_BIN' -p '$SSH_PORT $DATA_DIR $SSH_USER@$REMOTE_SERVER:$DATA_DIR 2>&1 | $TEE_BIN -a $LOG_PATH
 
 echo "Starting BackupPC..." 2>&1 | $TEE_BIN -a $LOG_PATH
@@ -59,4 +59,4 @@ rm -f $LOCK_FILE 2>&1 | $TEE_BIN -a $LOG_PATH
 echo "" 2>&1 | $TEE_BIN -a $LOG_PATH
 
 # Email the results to $EMAIL_TO
-echo -ne "$(head $LOG_PATH)\n\n[stripped...]\n\n$(tail $LOG_PATH)" | $MAIL_BIN -a "From: "$EMAIL_FROM -s "$EMAIL_SUBJECT" $EMAIL_TO 2>&1 | $TEE_BIN -a $LOG_PATH
+echo -ne "$(head -n20 $LOG_PATH)\n\n[stripped...]\n\n$(tail -n20 $LOG_PATH)" | $MAIL_BIN -a "From: "$EMAIL_FROM -s "$EMAIL_SUBJECT" $EMAIL_TO 2>&1 | $TEE_BIN -a $LOG_PATH
